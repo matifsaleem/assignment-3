6150cda2067fda57f6be9cc55ff2447d58593a61
github matifsaleem <atif_paf@hotmail.com>
2023-02-24 12:38:22 +0500

    Assignment # 3, Q-1

---
+++ Q-1.dart
@@ -0,0 +1,6 @@
+//Q.1: Create a list of names and print all names using list.
+
+void main(){
+  List<String> names = ['Muhammad Atif Saleem','Azhar Hussain','Irfan Raza Hussain'];
+  print(names);
+}
